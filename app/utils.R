cdcd28cece47a77f68a59bc502e3d905f3ce44cb
anguswg-ucsb <anguswatters@gmail.com>
2021-03-30 10:02:36 -0700

    climateR app v1

---
+++ app/utils.R
@@ -0,0 +1,70 @@
+basemap <- function() {
+  # pal = colorNumeric("inferno", reverse= TRUE, domain = today$size, n = 50)
+  # pal2 <- colorNumeric("inferno", reverse = TRUE, domain = today$cases, n = 50)
+
+  leaflet() %>%
+    addProviderTiles(providers$OpenStreetMap, group = "Topographic") %>%
+    addProviderTiles(providers$Esri.WorldShadedRelief, group = "Relief") %>%
+    addLayersControl(options = layersControlOptions(collapsed = FALSE),
+                     baseGroups = c('Topographic', "Relief")) %>%
+    addScaleBar("bottomleft") %>%
+    setView(-95,40,4) %>%
+    addMeasure(
+      position = "bottomleft",
+      primaryLengthUnit = "feet",
+      primaryAreaUnit = "sqmiles",
+      activeColor = "red",
+      completedColor = "green" ) %>%
+    leafem::addMouseCoordinates()
+}
+
+second_map <- function() {
+  # pal = colorNumeric("inferno", reverse= TRUE, domain = today$size, n = 50)
+  # pal2 <- colorNumeric("inferno", reverse = TRUE, domain = today$cases, n = 50)
+  leaflet() %>%
+    addProviderTiles(providers$Esri.WorldImagery) %>%
+    addScaleBar("bottomleft") %>%
+    setView(-95,40,4) %>%
+    addMiniMap(toggleDisplay = TRUE, minimized = FALSE) %>%
+    addMeasure(
+      position = "bottomleft",
+      primaryLengthUnit = "feet",
+      primaryAreaUnit = "sqmiles",
+      activeColor = "red",
+      completedColor = "green" ) %>%
+    leafem::addMouseCoordinates()
+}
+
+zoom_to_catch = function(map, df, catchment){
+  # Filter the counties to the input FIP code
+  shp = filter(df, comid == catchment)
+  # Build a buffered bounding box to center the map on:
+  bounds = shp %>%
+    # make bounding box
+    st_bbox() %>%
+    # Make spatial
+    st_as_sfc() %>%
+    # Buffer to .1 degree
+    st_buffer(.1) %>%
+    # make new bounding box
+    st_bbox() %>%
+    # extract coordinates as vector
+    as.vector()
+  # Clear all current shapes (remember county centroids are currently markers!)
+  clearShapes(map) %>%
+    # Add the county shape making the outline color red and the fill an opaque white
+    addPolygons(data = shp,
+                color = "red",
+                fillColor  = "grey",
+                fillOpacity = .3) %>%
+    # Fly the leaflet map to the buffered boundary
+    flyToBounds(bounds[1], bounds[2], bounds[3], bounds[4])
+}
+
+
+
+
+
+
+
+
